7e843a2f04639356c6eb7c8fb6e53b20a59f6148
Jonathan Scott <jonathan.scott@gmail.com>
2019-10-06 23:49:21 -0700

    first push project routes finished

---
+++ logger.js
@@ -0,0 +1,11 @@
+function logger(req, res, next) {
+    console.log(
+      `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+        'Origin'
+      )}`
+    );
+  
+    next();
+  }
+
+  module.exports = logger;
